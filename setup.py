56487d08b1c9852c0443a4400dfc9434d552f5d8
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2017-08-10 23:03:22 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+
+    from setuptools import setup, find_packages
+
+    setup(
+        install_requires = ['sportsteam'],
+        packages         = find_packages(),
+        name             = "tuhlaajapojat",
+        version          = "1.0",
+    )
+
---
+++ tuhlaajapojat/settings.py
@@ -0,0 +1,42 @@
+# Django settings for tuhlaajapojat project.
+
+from sportsteam.settings.prod import *
+
+DEBUG = False
+
+TEAM_NAME = 'FC Tuhlaajapojat'
+TEAM_SLUG = 'tuhlaajapojat'
+TEAM_TAG = 'Tuhlaajapojat'
+
+ADMINS = (
+    ('Jaakko Luttinen', 'jaakko.luttinen@iki.fi'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Use environment variable here..
+        'NAME':  os.getenv("TUHLAAJAPOJAT_DATABASE_NAME", "/tmp/tuhlaajapojat.db")
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Helsinki'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'fi'
+
+SITE_ID = 1
+
+# Make this unique, and don't share it with anybody.
+# Use environment variable here..
+SECRET_KEY = os.getenv("TUHLAAJAPOJAT_SECRET_KEY", "foobar")
+
+ALLOWED_HOSTS = ['*']
